04d56a32cd362a42ff70c5d3b7456ef19ff782a8
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-05-01 09:24:23 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1> 💼 Far Away 🌴 </h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip? </h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
